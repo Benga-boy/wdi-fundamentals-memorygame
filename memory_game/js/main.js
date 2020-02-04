9ffb153e569cd286f3c20af35435712b109cbaa9
WhosMacisthis <olugbengaolasebikan@macbook-pro.home>
2020-02-04 14:30:00 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,28 @@
-const cards = ['Queen', 'Queen', 'King', 'King'];
+let cards = ['Queen', 'Queen', 'King', 'King'];
+let cardsInPlay = [];
 
-const cardsInPlay = [];
-
-const cardOne = cards[0];
-
-const cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2); {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
+function checkForMatch() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	}
 	else {
 		alert("Sorry try again");
 	}
 }
+
+
+function flipCard(cardId) {
+	if (cardsInPlay.length === 2) {
+	checkForMatch();	
+}
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+}
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
+
+
 
 
