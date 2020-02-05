b314f82f91ed67c62e882d52e9a7247761c1a0bd
WhosMacisthis <olugbengaolasebikan@macbook-pro.home>
2020-02-05 17:11:57 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,19 +33,34 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 }
 
 
-function flipCard(cardId) {
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	console.log("User flipped " + cards[cardId].rank + " of " + cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 	checkForMatch();	
 }
-console.log("User flipped " + cards[cardId].rank);
-cardsInPlay.push(cards[cardId].rank);
-console.log(cards[cardId].cardImage);
-console.log(cards[cardId].suit);
+
 }
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+function createBoard() {
+	for (i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png' );
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
+
+
+
+
+
 
 
 
